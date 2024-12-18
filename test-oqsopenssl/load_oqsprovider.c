55330bd43cb75825b156d8d98604b7753a2450be
sus0pid <s2136500@ed.ac.uk>
2024-12-18 16:03:27 +0000

    add T macro

--- test-oqsopenssl/load_oqsprovider.c
+++ test-oqsopenssl/load_oqsprovider.c
@@ -1,8 +1,24 @@
+/*gcc -o dynamic_oqsprovider dynamic_oqsprovider.c -I/usr/local/include -L/usr/local/lib64 -lssl -lcrypto -ldl
+ */
 #include <stdio.h>
 #include <stdlib.h> // For setenv
 #include <openssl/crypto.h>
 #include <openssl/err.h>
 #include <openssl/provider.h>
+
+#define T(e)                                                                   \
+    if (!(e)) {                                                                \
+        ERR_print_errors_fp(stderr);                                           \
+        OPENSSL_die(#e, __FILE__, __LINE__);                                   \
+    }
+
+#define TF(e)                                                                  \
+    if ((e)) {                                                                 \
+        ERR_print_errors_fp(stderr);                                           \
+    } else {                                                                   \
+        OPENSSL_die(#e, __FILE__, __LINE__);                                   \
+    }
+
 
 OSSL_PROVIDER *load_default_provider(OSSL_LIB_CTX *libctx) {
     OSSL_PROVIDER *provider;
@@ -13,20 +29,12 @@ OSSL_PROVIDER *load_default_provider(OSSL_LIB_CTX *libctx) {
 static int load_oqs_provider(OSSL_LIB_CTX *libctx) {
     // Set the default provider search path (optional)
     const char *provider_path = "/usr/local/lib64/ossl-modules";
-    if (!OSSL_PROVIDER_set_default_search_path(libctx, provider_path)) {
-        fprintf(stderr, "Failed to set default provider search path: %s\n", provider_path);
-        ERR_print_errors_fp(stderr);
-        return -1;
-    }
+    T(OSSL_PROVIDER_set_default_search_path(libctx, provider_path));
     printf("Provider search path set to: %s\n", provider_path);
 
     // Load the OQS provider
     OSSL_PROVIDER *provider = OSSL_PROVIDER_load(libctx, "oqsprovider");
-    if (provider == NULL) {
-        fprintf(stderr, "Failed to load the OQS provider.\n");
-        ERR_print_errors_fp(stderr);
-        return -1;
-    }
+    T(provider != NULL);
     printf("OQS provider successfully loaded.\n");
     return 0;
 }
@@ -34,11 +42,7 @@ static int load_oqs_provider(OSSL_LIB_CTX *libctx) {
 int main() {
     // create a new openssl lib context
     OSSL_LIB_CTX *libctx = OSSL_LIB_CTX_new();
-    if (libctx == NULL) {
-        fprintf(stderr, "Failed to create OpenSSL library context.\n");
-        ERR_print_errors_fp(stderr);
-        return 1;
-    }
+    T (libctx != NULL);
 
     if (load_oqs_provider(libctx) != 0) {
         fprintf(stderr, "Failed to load OQS provider.\n");
